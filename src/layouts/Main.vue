293fa136ac56fc081261037c5202d35b34f5c7df
Robbe <robbevaes95@gmail.com>
2023-07-19 16:24:26 +0200

    Made an app

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="flex max-h-screen min-h-screen bg-neutral-100 p-2 lg:items-center lg:justify-center lg:px-12">
+  <main class="flex max-h-screen min-h-[100dvh] bg-neutral-100 p-2 lg:min-h-screen lg:items-center lg:justify-center lg:px-12">
     <router-view />
   </main>
 </template>
